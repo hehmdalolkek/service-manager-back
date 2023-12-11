ed0adbb54818a8db114fa43306ae31c1bc5e6ff6
hehmdalolkek <badeha.inna@yandex.ru>
2023-12-11 14:59:17 +0400

    Added entities

---
+++ src/main/java/com/hehmdalolkek/spring/servicemanagerback/entity/Category.java
@@ -0,0 +1,40 @@
+package com.hehmdalolkek.spring.servicemanagerback.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotBlank
+    @Size(min = 2, max = 128)
+    @Column(name = "title")
+    private String title;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/hehmdalolkek/spring/servicemanagerback/entity/Service.java
@@ -0,0 +1,92 @@
+package com.hehmdalolkek.spring.servicemanagerback.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "services")
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    @NotBlank
+    @Size(min = 2, max = 128)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "category", referencedColumnName = "id")
+    private Category category;
+
+    @Column(name = "duration")
+    @Min(0)
+    private int duration;
+
+    @Column(name = "price")
+    @Min(0)
+    private double price;
+
+    @Column(name = "change_date")
+    @Past
+    private Timestamp change_date;
+
+
+    public Service() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Timestamp getChange_date() {
+        return change_date;
+    }
+
+    public void setChange_date(Timestamp change_date) {
+        this.change_date = change_date;
+    }
+}
