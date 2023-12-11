50db678ee1b6133056be1291635bc0c8b39a79a5
hehmdalolkek <badeha.inna@yandex.ru>
2023-12-11 17:30:11 +0400

    Added CategoryRepository

--- src/main/java/com/hehmdalolkek/spring/servicemanagerback/dao/CategoryRepository.java
+++ src/main/java/com/hehmdalolkek/spring/servicemanagerback/dao/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.hehmdalolkek.spring.servicemanagerback.dao;
 
 import com.hehmdalolkek.spring.servicemanagerback.entity.Category;
+import com.hehmdalolkek.spring.servicemanagerback.entity.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
