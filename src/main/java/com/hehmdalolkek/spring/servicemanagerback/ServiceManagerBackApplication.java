588b4a94cebfc39c48932912cebeff432f30d5e1
hehmdalolkek <badeha.inna@yandex.ru>
2023-12-11 14:26:20 +0400

    initial commit

---
+++ src/main/java/com/hehmdalolkek/spring/servicemanagerback/ServiceManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.servicemanagerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hehmdalolkek/spring/servicemanagerback/ServiceManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hehmdalolkek.spring.servicemanagerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
